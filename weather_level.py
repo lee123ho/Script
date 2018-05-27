a27ee1e226eae6d83ed62c5286d399583e05d6e8
lee123ho <32889136+lee123ho@users.noreply.github.com>
2018-05-28 07:41:58 +0900

    Add files via upload

--- weather_level.py
+++ weather_level.py
@@ -5,17 +5,18 @@ import xml.etree.ElementTree as ET
 
 
 areaNo = None
+timeNo = None
 
 def Asthma():
     url = "http://newsky2.kma.go.kr/iros/RetrieveWhoIndexService2/getAsthmaWhoList?"
     key = "serviceKey=G0ggqSF4z2N5hBVSDJu9WW5KknYmrBgA4rjnlOGWp32SYNBIvM55VGV%2FoikjOSONkX8TmT6CkncrTZJDm5QVcw%3D%3D"
-    area = "&areaNo=" + "1100000000"
-    time = "&time=" + "2018052706"
+    area = "&areaNo=" + areaNo
+    time = "&time=" + timeNo
     api_url = url + key + area + time
 
     data = urllib.request.urlopen(api_url).read()
 
-    filename = "sample2.xml"
+    filename = "Asthma.xml"
     f = open(filename, "wb")
     f.write(data)
     f.close()
@@ -35,13 +36,13 @@ def Asthma():
 def Brain():
     url = "http://newsky2.kma.go.kr/iros/RetrieveWhoIndexService2/getBrainWhoList?"
     key = "serviceKey=G0ggqSF4z2N5hBVSDJu9WW5KknYmrBgA4rjnlOGWp32SYNBIvM55VGV%2FoikjOSONkX8TmT6CkncrTZJDm5QVcw%3D%3D"
-    area = "&areaNo=" + "1100000000"
-    time = "&time=" + "2018052706"
+    area = "&areaNo=" + areaNo
+    time = "&time=" + timeNo
     api_url = url + key + area + time
 
     data = urllib.request.urlopen(api_url).read()
 
-    filename = "sample2.xml"
+    filename = "Brain.xml"
     f = open(filename, "wb")
     f.write(data)
     f.close()
@@ -61,13 +62,13 @@ def Brain():
 def Skin():
     url = "http://newsky2.kma.go.kr/iros/RetrieveWhoIndexService2/getSkinWhoList?"
     key = "serviceKey=G0ggqSF4z2N5hBVSDJu9WW5KknYmrBgA4rjnlOGWp32SYNBIvM55VGV%2FoikjOSONkX8TmT6CkncrTZJDm5QVcw%3D%3D"
-    area = "&areaNo=" + "1100000000"
-    time = "&time=" + "2018052706"
+    area = "&areaNo=" + areaNo
+    time = "&time=" + timeNo
     api_url = url + key + area + time
 
     data = urllib.request.urlopen(api_url).read()
 
-    filename = "sample2.xml"
+    filename = "Skin.xml"
     f = open(filename, "wb")
     f.write(data)
     f.close()
@@ -86,6 +87,9 @@ def Skin():
 
 if __name__ == "__main__":
     areaNo = input('지점 번호 : ')
+    print("측정 시간은 06, 18 ex)2018052706 2018년5월27일6시")
+    timeNo = input("시간 : ")
+    print("0 - 낮음 1 - 보통 2 - 높음 3 - 매우높음")
     Asthma()
     Brain()
     Skin()
